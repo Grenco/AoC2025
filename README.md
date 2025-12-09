146d68dc5e3e223a070210731190d981e1dec292
Grenco <graemesmyrl@gmail.com>
2025-12-09 08:12:20 +0000

    Day 1 Part 2

--- README.md
+++ README.md
@@ -11,5 +11,8 @@ As you may notice from the commit, it's December 3rd. I'd like to say I've been 
 
 In my defence, Zig [recently changed how readers and writers work](https://ziglang.org/download/0.15.1/release-notes.html#New-stdIoWriter-and-stdIoReader-API), which meant most information online was old and this took me way too long to figure out.
 
-After getting that right, it was (mostly) smooth sailing when I figured out the error system and the convoluted way to [concat strings](https://www.xeg.io/shared-searches/a-comprehensive-guide-to-working-with-strings-in-zig-programming-language-666df7a25ccf5bab1ec23e5c#:~:text=print(%22%7B%7D%5Cn%22%2C%20.%7Bsubstring%7D)%3B%0A%7D-,String%20Concatenation,-Zig%20does%20not).
+After getting that right, it was (mostly) smooth sailing when I figured out the error system and the convoluted way to [concat strings](<https://www.xeg.io/shared-searches/a-comprehensive-guide-to-working-with-strings-in-zig-programming-language-666df7a25ccf5bab1ec23e5c#:~:text=print(%22%7B%7D%5Cn%22%2C%20.%7Bsubstring%7D)%3B%0A%7D-,String%20Concatenation,-Zig%20does%20not>).
 
+### Part 2
+
+Okay, this time I actually have been busy but I'm finally finding time to work on this again.
--- src/day1/solution.zig
+++ src/day1/solution.zig
@@ -1,14 +1,19 @@
 const std = @import("std");
 const expect = std.testing.expect;
 
-pub fn findCombination(fileName: []const u8) anyerror!u32 {
+fn getFile(fileName: []const u8) !std.fs.File {
     const parts = [_][]const u8{ "src/day1/", fileName };
     const allocator = std.heap.page_allocator;
     const full_path = std.mem.concat(allocator, u8, &parts) catch unreachable;
-    std.debug.print("PATH {s}", .{full_path});
+
     defer allocator.free(full_path);
 
     const input_file = try std.fs.cwd().openFile(full_path, .{});
+    return input_file;
+}
+
+fn findCombination(fileName: []const u8, part2: bool) anyerror!u32 {
+    const input_file = try getFile(fileName);
     defer input_file.close();
 
     var buffer: [1024]u8 = undefined;
@@ -16,6 +21,7 @@ pub fn findCombination(fileName: []const u8) anyerror!u32 {
 
     var dial_position: i16 = 50;
     var zero_count: u16 = 0;
+    var passed_zero_count: u16 = 0;
 
     while (reader.interface.takeDelimiterInclusive('\n')) |line| {
         const direction = line[0];
@@ -28,38 +34,65 @@ pub fn findCombination(fileName: []const u8) anyerror!u32 {
         const num_string = line[1 .. line.len - 1];
 
         const value = try std.fmt.parseInt(i16, num_string, 10);
+
+        const was_on_zero: u8 = if (dial_position == 0 and multiplier == -1) 1 else 0;
+
         dial_position += multiplier * value;
-        while (dial_position < 0) {
-            dial_position += 100;
-        }
+
+        const on_zero: u8 = if (dial_position == 0) 1 else 0;
+        const zero_ticks = @abs(@divFloor(dial_position, 100));
+
         dial_position = @mod(dial_position, 100);
+
+        const is_negative_zero: u8 = if (dial_position == 0 and multiplier == -1 and value >= 100) 1 else 0;
+
+        passed_zero_count += zero_ticks - was_on_zero + on_zero + is_negative_zero;
 
         if (dial_position == 0) {
             zero_count += 1;
         }
     } else |err| switch (err) {
-        error.EndOfStream => return zero_count, // stream ended not on a line break
+        error.EndOfStream => return if (part2) passed_zero_count else zero_count, // stream ended not on a line break
         error.StreamTooLong, // line could not fit in buffer
         error.ReadFailed, // caller can check reader implementation for diagnostics
         => |e| return e,
     }
-    return zero_count;
+    return if (part2) passed_zero_count else zero_count;
 }
 
+const example_only = false;
 test "Day 1 Part 1 Example" {
-    const result = findCombination("exampleInput.txt") catch |err| {
+    const result = findCombination("exampleInput.txt", false) catch |err| {
         std.debug.print("{s}", .{@errorName(err)});
         return;
     };
-    std.debug.print("EXAMPLE RESULT: {d} \n", .{result});
+    std.debug.print("Pt.1 EXAMPLE RESULT: {d} \n", .{result});
     try expect(result == 3);
 }
-
 test "Day 1 Part 1" {
-    const result = findCombination("input.txt") catch |err| {
+    if (example_only) return;
+    const result = findCombination("input.txt", false) catch |err| {
         std.debug.print("{s}", .{@errorName(err)});
         return;
     };
-    std.debug.print("RESULT: {d} \n", .{result});
-    try expect(result > 0);
+    std.debug.print("Pt.1 RESULT: {d} \n", .{result});
+    try expect(result == 995);
+}
+
+test "Day 1 Part 2 Example" {
+    const result = findCombination("exampleInput.txt", true) catch |err| {
+        std.debug.print("{s}", .{@errorName(err)});
+        return;
+    };
+    std.debug.print("Pt.2 EXAMPLE RESULT: {d} \n", .{result});
+    try expect(result == 6);
+}
+test "Day 1 Part 2" {
+    if (example_only) return;
+    const result = findCombination("input.txt", true) catch |err| {
+        std.debug.print("{s}", .{@errorName(err)});
+        return;
+    };
+    std.debug.print("Pt.2 RESULT: {d} \n", .{result});
+    try expect(result == 5847);
 }
